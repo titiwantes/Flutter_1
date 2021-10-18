09916013960fdb77a1fb5107504add8a5af6fb25
arthur.mariet@epitech.eu <arthur.mariet@epitech.eu>
2021-10-18 23:59:42 +0200

    Help page v2

--- app/lib/modules/Help/Help.dart
+++ app/lib/modules/Help/Help.dart
@@ -1,54 +1,104 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-// Create a Form widget.
+//import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+//import 'package:provider/provider.dart';
+import 'package:app/config/CustomTextStyle.dart';
+import 'package:app/config/Colors.dart';
+import 'package:app/config/routes.dart';
+//import 'package:app/core/api/ZephyAPI.dart';
+//import 'package:app/core/api/models/Parameters.dart';
+//import 'package:app/core/api/models/User.dart';
+//import 'package:app/core/states/UserState.dart';
+import 'package:app/shared/MainButton.dart';
+import 'package:app/shared/MainInput.dart';
+import 'package:app/shared/SubmitInput.dart';
+import 'package:app/shared/Separator.dart';
+import 'package:app/shared/button-style/ButtonStyle1.dart';
+import 'package:app/shared/header-style/HeaderStyle1.dart';
+import 'package:app/utils/Arguments.dart';
+import 'package:app/utils/Percentage.dart';
+//import 'package:app/utils/services/SecureStorageService.dart';
+import 'package:app/utils/Alert.dart';
+import 'package:app/utils/routes-animation/slideLeftRoute.dart';
+
 class Help extends StatefulWidget {
-  const Help({Key? key}) : super(key: key);
+  Help({Key? key, this.args}) : super(key: key);
+
+  Arguments? args;
 
   @override
-  MyCustomFormState createState() {
-    return MyCustomFormState();
-  }
+  State<Help> createState() => _LoginPageState();
 }
 
-// Create a corresponding State class.
-// This class holds data related to the form.
-class MyCustomFormState extends State<Help> {
-  // Create a global key that uniquely identifies the Form widget
-  // and allows validation of the form.
-  //
-  // Note: This is a GlobalKey<FormState>,
-  // not a GlobalKey<MyCustomFormState>.
-  final _formKey = GlobalKey<FormState>();
+class _LoginPageState extends State<Help> {
+  TextEditingController _email = TextEditingController();
+  TextEditingController _password = TextEditingController();
+
+  ///
+  /// Called when user click on connect button.
+  ///
+  ///
+  /// Called when user click on connect button.
+  ///
+  void _onClickConnect() {
+    Navigator.pushNamed(context, '/');
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    // Build a Form widget using the _formKey created above.
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          TextFormField(
-            // The validator receives the text that the user has entered.
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                // Validate returns true if the form is valid, or false otherwise.
-                print("Message is submitted");
-              },
-              child: const Text('Submit'),
+    //return Consumer<UserState>(
+      //builder: (context, user, child) {
+        return Scaffold(
+          backgroundColor: Colors.white,
+          body: SingleChildScrollView(
+            padding: EdgeInsets.only(left: Percentage.width(context, 3), right: Percentage.width(context, 3), top: 20),
+            child: Column(
+              children: <Widget>[
+                Text("Contact Us", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                Container(
+                  margin: EdgeInsets.only(top: 40),
+                  child: MainInput(
+                    key: Key("Input email connect"),
+                    textPlaceholder: "Your message title...",
+                    showIcon: false,
+                    fontSizeInput: 15,
+                    label: "Title",
+                    inputBorder: Border.all(color: Pcolors.transparentGrey),
+                    onValidate: (String text) {
+                    },
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 20),
+                  child: SubmitInput(
+                    key: Key("Input password connect"),
+                    textPlaceholder: "Type your message...",
+                    showIcon: false,
+                    fontSizeInput: 15,
+                    label: "Message",
+                    inputBorder: Border.all(color: Pcolors.transparentGrey),
+                    onValidate: (String text) {
+                    },
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 30),
+                  child: MainButton(
+                    key: Key("Button connect"),
+                    text: "SUBMIT MESSAGE",
+                    color: Pcolors.blue,
+                    onTap: () {
+                      print("Message submited");
+                    },
+                  ),
+                ),
+              ],
             ),
           ),
-        ],
-      ),
-    );
-  }
+        );
+      }
+//    );
+//  }
 }
